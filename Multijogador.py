3c866b5f6695ba802c901b02afe60156a5184062
brunolleite <brunoll3@al.insper.edu.br>
2016-05-11 09:25:36 -0300

    Update Multijogador.py

--- Multijogador.py
+++ Multijogador.py
@@ -1,103 +1,176 @@
 import tkinter as tk
 
+import time
+
 class Multijogador:
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Modo multijogador")
-        self.window.geometry("2000x2000")
-        self.window.configure(background = 'deep pink')
-        self.window.rowconfigure(0, minsize=10, weight=1)
-        self.window.rowconfigure(1, minsize=10, weight=1)
-        self.window.rowconfigure(2, minsize=10, weight=1)
-        self.window.rowconfigure(3, minsize=10, weight=1)
-        self.window.rowconfigure(4, minsize=10, weight=1)
-        self.window.rowconfigure(5, minsize=10, weight=1)
-        self.window.rowconfigure(6, minsize=10, weight=1)
-        self.window.rowconfigure(7, minsize=10, weight=1)
+        self.window_multijogador = tk.Tk()
+        self.window_multijogador.config(background = 'black')
+        self.window_multijogador.title("Modo multijogador")
+        self.window_multijogador.geometry("700x700")
         
+        self.window_multijogador.rowconfigure(0, minsize=10, weight=1)
+        self.window_multijogador.rowconfigure(1, minsize=10, weight=1)
+        self.window_multijogador.rowconfigure(2, minsize=10, weight=1)
+        self.window_multijogador.rowconfigure(3, minsize=10, weight=1)
+        self.window_multijogador.rowconfigure(4, minsize=10, weight=1)
+        self.window_multijogador.rowconfigure(5, minsize=10, weight=1)
+        self.window_multijogador.rowconfigure(6, minsize=10, weight=1)
         
+        self.window_multijogador.columnconfigure(0, minsize=100, weight=1)
+        self.window_multijogador.columnconfigure(1, minsize=100, weight=1)
+        self.window_multijogador.columnconfigure(2, minsize=100, weight=1)
+        self.window_multijogador.columnconfigure(3, minsize=100, weight=1)
+        self.window_multijogador.columnconfigure(4, minsize=100, weight=1)
+        self.window_multijogador.columnconfigure(5, minsize=100, weight=1)
         
-        
+        self.nivel = 0
+        self.iniciar = False
+        self.cria_jogada=[]
+        self.jogada_usuario=[]
+        self.botao_clicado = False
+        self.tempo = 1
+        self.numero_botao = 0
+        self.lista_real=[]
+        self.lista_jogador=[]
 
-        self.window.columnconfigure(0, minsize=100, weight=1)
-        self.window.columnconfigure(1, minsize=100, weight=1)
-        self.window.columnconfigure(2, minsize=100, weight=1)
-        self.window.columnconfigure(3, minsize=100, weight=1)
-        self.window.columnconfigure(4, minsize=100, weight=1)
-        self.window.columnconfigure(5, minsize=100, weight=1)
-        self.window.columnconfigure(6, minsize=100, weight=1)
-        self.window.columnconfigure(7, minsize=100, weight=1)
-        self.window.columnconfigure(8, minsize=100, weight=1)
-        self.window.columnconfigure(9, minsize=100, weight=1)
-        self.window.columnconfigure(10, minsize=100, weight=1)
+        self.pontos = tk.Label(self.window_multijogador)
+        self.pontos.grid(row=6, column=4, columnspan=2, sticky="nsew")
+        self.pontos.configure(text="NÍVEL: {0}".format(self.nivel), font='Broadway 18', background='black', fg = 'cyan')
         
-        
+        self.titulo = tk.Label(self.window_multijogador)
+        self.titulo.grid(row=0, column=1, columnspan=4, sticky="nsew")
+        self.titulo.configure(text = 'MULTIJOGADOR', font='Broadway 64', fg = 'cyan', background='black')
 
-        self.pontos = tk.Label(self.window)
-        self.pontos.grid(row=1, column=3, columnspan=2, sticky="nsew")
-        self.pontos.configure(text="Pontos:", font='Symbol 30', background='snow')
-        
-        self.pontos = tk.Label(self.window)
-        self.pontos.grid(row=1, column=8, columnspan=2, sticky="nsew")
-        self.pontos.configure(text="Pontos:", font='Symbol 30', background='snow')
-        
-        self.jogador1 = tk.Label(self.window)
-        self.jogador1.grid(row=1, column=1, columnspan=2, sticky="nsew")
-        self.jogador1.configure(text="Jogador 1:", font='Symbol 30', background='snow')
-        
-        self.jogador2 = tk.Label(self.window)
-        self.jogador2.grid(row=1, column=6, columnspan=2, sticky="nsew")
-        self.jogador2.configure(text="Jogador 2:", font='Symbol 30', background='snow')
-        
-        self.titulo = tk.Label(self.window)
-        self.titulo.grid(row=0, column=4, columnspan=4, sticky="nsew")
-        self.titulo.configure(text="Multijogador", font='Symbol 67', background='deep pink')
-
-        self.botão0 = tk.Button(self.window)
-        self.botão0.grid(row=2, column=1, columnspan=2, rowspan=2, sticky="nsew")
-        self.botão0.configure(background = 'yellow')
+        self.botao0 = tk.Button(self.window_multijogador)
+        self.botao0.grid(row=1, column=1, columnspan=2, rowspan=2, sticky="nsew")
+        self.botao0.configure(activebackground = 'pale goldenrod', background = 'yellow', borderwidth=12, command = self.click_botao0)
          
-        self.botão1 = tk.Button(self.window)
-        self.botão1.grid(row=4, column=1, columnspan=2, rowspan=2, sticky="nsew")
-        self.botão1.configure(background = 'green2')
+        self.botao1 = tk.Button(self.window_multijogador)
+        self.botao1.grid(row=3, column=1, columnspan=2, rowspan=2, sticky="nsew")
+        self.botao1.configure(activebackground = 'pale green', background = 'green2', borderwidth=12, command = self.click_botao1)
           
-        self.botão2 = tk.Button(self.window)
-        self.botão2.grid(row=2, column=3, columnspan=2, rowspan=2, sticky="nsew")
-        self.botão2.configure(background = 'red')
+        self.botao2 = tk.Button(self.window_multijogador)
+        self.botao2.grid(row=1, column=3, columnspan=2, rowspan=2, sticky="nsew")
+        self.botao2.configure(activebackground = 'tomato', background = 'red', borderwidth=12, command = self.click_botao2)
 
-        self.botão3 = tk.Button(self.window)
-        self.botão3.grid(row=4, column=3, columnspan=2, rowspan=2, sticky="nsew")
-        self.botão3.configure(background = 'blue')
+        self.botao3 = tk.Button(self.window_multijogador)
+        self.botao3.grid(row=3, column=3, columnspan=2, rowspan=2, sticky="nsew")
+        self.botao3.configure(activebackground = 'sky blue', background = 'blue', borderwidth=12, command = self.click_botao3)
         
+        self.botaoiniciar = tk.Button(self.window_multijogador)
+        self.botaoiniciar.grid(row=6, column=2, columnspan = 2, sticky="nsew")
+        self.botaoiniciar.configure(relief = 'ridge', text="INICIAR", borderwidth=6, activebackground = 'green2', background='black', fg = 'cyan', font='Broadway 16', command = self.botaoiniciar_teste)
+          
+        self.botaomenu = tk.Button(self.window_multijogador)
+        self.botaomenu.grid(row=6, column=0, columnspan = 2, sticky="nsew")
+        self.botaomenu.configure(relief = 'ridge', text="MENU", borderwidth=6, activebackground = 'green2', background='black', fg = 'cyan', font='Broadway 16')   
         
-        self.botão4 = tk.Button(self.window)
-        self.botão4.grid(row=2, column=6, columnspan=2, rowspan=2, sticky="nsew")
-        self.botão4.configure(background = 'yellow')
+    def botaoiniciar_teste(self):
+        self.botaoiniciar.destroy()        
+    
+    def click_botao0(self):
+        while len(self.lista_real)>len(self.lista_jogador):
+            self.lista_jogador.append(0)
+            self.verifica_jogadas()
+        self.lista_real.append(0)
+        
+    def click_botao1(self):
+        while len(self.lista_real)>len(self.lista_jogador):
+            self.lista_jogador.append(1)
+            self.verifica_jogadas()
+        self.lista_real.append(1)
+        
+    def click_botao2(self):
+        while len(self.lista_real)>len(self.lista_jogador):
+            self.lista_jogador.append(2)
+            self.verifica_jogadas()
+        self.lista_real.append(2)
+        
+    def click_botao3(self):
+        while len(self.lista_real)>len(self.lista_jogador):
+             self.lista_jogador.append(3)
+             self.verifica_jogadas()
+        self.lista_real.append(3)
+    
+    def piscar(self):
+        for i in self.lista_real:
+            if i == 0:
+                self.botao0.configure(background = 'pale goldenrod')
+                self.window_multijogador.update()
+                time.sleep(self.tempo * 0.70)
+                self.botao0.configure(background = 'yellow')
+            elif i == 1:
+                self.botao1.configure(background = 'pale green')
+                self.window_multijogador.update()
+                time.sleep(self.tempo * 0.70)
+                self.botao1.configure(background = 'green2')
+            elif i == 2:
+                self.botao2.configure(background = 'tomato')
+                self.window_multijogador.update()
+                time.sleep(self.tempo * 0.70)
+                self.botao2.configure(background = 'red')
+            elif i == 3:
+                self.botao3.configure(background = 'sky blue')
+                self.window_multijogador.update()
+                time.sleep(self.tempo * 0.70)
+                self.botao3.configure(background = 'blue')
+            self.window_multijogador.update()
+            time.sleep(self.tempo)
+            
+    def verifica_jogadas(self):
+        if self.lista_jogador[self.numero_botao] != self.lista_real[self.numero_botao]:
+            self.lista_real = []
+            self.lista_jogador = []
+            self.erro()
+            print("Errou")
+        self.numero_botao += 1
+        self.window_multijogador.update()
+        if self.numero_botao == len(self.lista_real):  
+            time.sleep(1)
+            self.tempo = self.tempo * 0.90
+            print(self.tempo)
+            self.nivel += 1
+            self.jogada_usuario = []
+    
+    def erro(self):
+        self.tempo = 1
+        self.botao0.destroy()
+        self.botao1.destroy()
+        self.botao2.destroy()
+        self.botao3.destroy()
+        self.perdeu = tk.Label(self.window_multijogador)
+        self.perdeu.grid(row=1, column=1, columnspan=4, rowspan=4, sticky="nsew")
+        self.perdeu.configure(text="VOCÊ PERDEU...", font='Broadway 42', background='black', fg = 'cyan')
+        self.botaoreiniciar = tk.Button(self.window_multijogador)
+        self.botaoreiniciar.grid(row=6, column=2, columnspan = 2, sticky="nsew")
+        self.botaoreiniciar.configure(relief = 'ridge', text="REINICIAR", borderwidth=6, activebackground = 'green2', background='black', fg = 'cyan', font='Broadway 16')
+        self.botaoreiniciar.configure(command =lambda: self.botaoreiniciar_teste())
+
+    def botaoreiniciar_teste(self):
+        self.botao0 = tk.Button(self.window_multijogador)
+        self.botao0.grid(row=1, column=1, columnspan=2, rowspan=2, sticky="nsew")
+        self.botao0.configure(activebackground = 'pale goldenrod', background = 'yellow', borderwidth=12, command = self.click_botao0)
          
-        self.botão5 = tk.Button(self.window)
-        self.botão5.grid(row=4, column=6, columnspan=2, rowspan=2, sticky="nsew")
-        self.botão5.configure(background = 'green2')
+        self.botao1 = tk.Button(self.window_multijogador)
+        self.botao1.grid(row=3, column=1, columnspan=2, rowspan=2, sticky="nsew")
+        self.botao1.configure(activebackground = 'pale green', background = 'green2', borderwidth=12, command = self.click_botao1)
           
-        self.botão6 = tk.Button(self.window)
-        self.botão6.grid(row=2, column=8, columnspan=2, rowspan=2, sticky="nsew")
-        self.botão6.configure(background = 'red')
+        self.botao2 = tk.Button(self.window_multijogador)
+        self.botao2.grid(row=1, column=3, columnspan=2, rowspan=2, sticky="nsew")
+        self.botao2.configure(activebackground = 'tomato', background = 'red', borderwidth=12, command = self.click_botao2)
 
-        self.botão7 = tk.Button(self.window)
-        self.botão7.grid(row=4, column=8, columnspan=2, rowspan=2, sticky="nsew")
-        self.botão7.configure(background = 'blue')
+        self.botao3 = tk.Button(self.window_multijogador)
+        self.botao3.grid(row=3, column=3, columnspan=2, rowspan=2, sticky="nsew")
+        self.botao3.configure(activebackground = 'sky blue', background = 'blue', borderwidth=12, command = self.click_botao3)
         
-        self.botãoiniciar = tk.Button(self.window)
-        self.botãoiniciar.grid(row=7, column=0, sticky="nsew")
-        self.botãoiniciar.configure(text="Iniciar", font='Symbol 30')
-          
+        self.window_multijogador.update()
+        time.sleep(0.5)
 
-        self.botãomenu = tk.Button(self.window)
-        self.botãomenu.grid(row=7, column=1, sticky="nsew")
-        self.botãomenu.configure(text="Menu", font='Symbol 30')
+        
+         
+    def quit(self):
+        self.window.destroy()
   
-    def iniciar(self):
-        self.window.mainloop() 
-
-projeto = Multijogador()
-projeto.iniciar()
-
+    def iniciar_multijogador(self):
+        self.window_multijogador.mainloop() 
